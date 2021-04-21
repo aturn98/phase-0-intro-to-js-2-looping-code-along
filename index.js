7f277906e4aa2f5c12bea61f5d05dc4a33a50d74
Austin <aturn9880@gmail.com>
2021-04-20 21:24:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let completedCards = [];
+  for (let i = 0; i < names.length; i++) {
+    completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return completedCards;
+}
+
+function countDown() {
+  let number = 10;
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
